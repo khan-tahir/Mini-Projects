ce027061ee23167b18c6c7da582715184c411b62
khan-tahir <140940172+khan-tahir@users.noreply.github.com>
2023-07-30 13:03:37 +0530

    max_min.py

---
+++ max_min.py
@@ -0,0 +1,8 @@
+def max_min(num):
+  maximum_num = max(num)
+  minimum_num = min(num)
+  print(f"Maximum Number is {maximum_num})
+  print(f"Minimum Number is {minimum_num})
+
+x = [1,3,45,23,67,85,32,2,89,6,9,34,27]
+max_min(x)
