0180ddb7154684b86f2f57b374d65f9b0498c694
khan-tahir <140940172+khan-tahir@users.noreply.github.com>
2023-08-02 20:53:13 +0530

    Add files via upload

---
+++ numbertowords.py
@@ -0,0 +1,7 @@
+from num2words import num2words
+
+num = int(input("Enter the number to convert it into word : "))
+
+numtoword = num2words(num)
+
+print(f"Entered number {num} in words is {numtoword}")
