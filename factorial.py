35d5e5baaf3a1486617dfe2192f555a3a03c3277
khan-tahir <140940172+khan-tahir@users.noreply.github.com>
2023-08-07 21:14:16 +0530

    Add files via upload
    Finding Factorial Using Recursion

---
+++ factorial.py
@@ -0,0 +1,8 @@
+def factorial(num):
+    if num == 0 or num == 1:
+        return 1
+    else:
+        return (num*factorial(num-1))
+    
+user_input = int(input("Enter a number to find it's factorial : "))
+print(factorial(user_input))
